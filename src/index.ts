498c1f79de700b5c6b80ed684813115a1c23487c
Juan Paz <jpaz.dev94@gmail.com>
2026-03-25 15:19:28 -0300

    creando utils cookie.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express, { Application } from "express";
 import authRoutes from "./routes/auth.routes";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app: Application = express();
@@ -8,12 +9,14 @@ const port = 3000;
 app.use(
   cors({
     origin: "http://localhost:5173",
+    credentials:true, // Exigimos al front que nos envie credenciales, para recibir las cookies
     methods: ["GET", "POST"],
   }),
 );
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,18 +2,22 @@
 import { Request, Response, NextFunction } from 'express';
 import { verifyToken } from '../utils/jwt';
 
+//Middleware protect.auth
 export const protect = (req: Request, res: Response, next: NextFunction) => {
-    const authHeader = req.headers.authorization || ' ';
-    const [scheme, token] = authHeader.split(' ');
+    // const authHeader = req.headers.authorization || ' ';
+    // const [scheme, token] = authHeader.split(' ');
 
-    if (scheme !== 'Bearer' || !token) {
-        return res.status(401).json({ status:401, message: 'Missing or invalid Authorization header' });
-    }
+    // if (scheme !== 'Bearer' || !token) {
+    //     return res.status(401).json({ status:401, message: 'Missing or invalid Authorization header' });
+    // }
+    const token = req.cookies.access_token
+
+    if(!token) return res.status(401).json({ status:401, message: 'Missing or invalid Authorization header' });
 
     try {
         const decoded = verifyToken(token);
         
-        req.body = { id: decoded.id }
+        (req as any).user = { id: decoded.id }
         next();
     } catch (err) {
         res.status(401).json({ status:401, message: 'Invalid or expired token' });
