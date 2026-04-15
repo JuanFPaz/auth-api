1c28bf90b4f364fd56ac29d735eb1304e81f3b33
Juan Paz <jpaz.dev94@gmail.com>
2026-04-15 02:26:52 -0300

    Adaptando el jwt.ts al nuevo estilo (?

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,21 +1,21 @@
-// src/utils/jwt.ts
-import dotenv from 'dotenv';
-import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import { UserPayload } from "../types/user.types";
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET!;
-const EXPIRES_IN = '15m'; // short lifespan for security, extend if needed
+const EXPIRES_IN = "15m"; // short lifespan for security, extend if needed
 
-export const signToken = (payload: object): string => {
-    if (!JWT_SECRET) {
-        throw new Error('JWT_SECRET is not defined in environment variables');
-    }
-    return jwt.sign(payload, JWT_SECRET, {
-        expiresIn: EXPIRES_IN,
-        algorithm: 'HS512', // strong algorithm
-    });
+export const signToken = (payload: UserPayload) => {
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+  return jwt.sign(payload, JWT_SECRET, {
+    expiresIn: EXPIRES_IN,
+    algorithm: "HS512", // strong algorithm
+  });
 };
 
 export const verifyToken = (token: string): any => {
-    return jwt.verify(token, JWT_SECRET);
+  return jwt.verify(token, JWT_SECRET);
 };
