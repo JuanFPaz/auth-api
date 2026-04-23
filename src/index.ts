6880925207b1982237ab9be955ba34dd6590e04c
Juan Paz <jpaz.dev94@gmail.com>
2026-04-22 23:49:35 -0300

    agregando en cors los metodos DELETE y PATCH

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT ?? 3000;
 app.use(
   cors({
     origin:['http://localhost:5173','https://juanfpaz.github.io'],
-    methods:['GET','POST'],
+    methods:['GET','POST','DELETE','PATCH'],
     credentials:true
   }),
 );
